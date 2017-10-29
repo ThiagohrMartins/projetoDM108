dd9d9a26f14e7c620d8d40a05d90020458dacc77
ThiagohrMartins <thiagomartins85@gmail.com>
2017-10-29 21:00:40 -0200

    Projeto DM108

--- platforms/android/assets/www/js/registerModel.js
+++
@@ -1,5 +0,0 @@
-function Register(date, time, status){
-    this.date = date;
-    this.time = time;
-    this.status = status;
-}
--- platforms/browser/www/js/userModel.js
+++
@@ -1,9 +0,0 @@
-function User(name, email, password, phone, gender, birth, location ) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.phone = phone;
-    this.gender = gender;
-    this.birth = birth;
-    this.location = location;
-}
